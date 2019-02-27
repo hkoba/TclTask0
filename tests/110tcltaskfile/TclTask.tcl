bf18ea6d4ff62f07d4310eed9dcdec66042fcd1c
Kobayasi, Hiroaki <buribullet@gmail.com>
2019-02-27 16:00:09 +0900

    Add more comments in sample TclTask.tcl

--- tests/110tcltaskfile/TclTask.tcl
+++ tests/110tcltaskfile/TclTask.tcl
@@ -1,62 +1,103 @@
 #!/usr/bin/env TclTaskRunner.tcl
 # -*- coding: utf-8 -*-
 
-proc cc args {exec gcc {*}$args}
+# TclTask.tcl file is sourced in [namespace eval ::],
+# from [apply] with some snit-specific arguments ($self, $type and $selfns).
 
+
+#
+# [dep] is a toplevel TclTaskRunner object. You can refere it as $self.
+#
+# dep add $TARGET $DEPENDS $ACTION
+#
 dep add a.o {a.c a.h} {cc -c $< -o $@}
 
-foreach k {b c} {
-    dep add $k.o [list $k.c $k.h] {cc -c $< -o $@}
-}
+# $ACTION can contain arbitrary tcl script.
+# It also supports following Makefile equivalent of shorthand variables:
+#
+# $@  target
+# $<  dependency list
+# $^  first element of dependency list
 
-dep add main.o {main.c a.h b.h c.h} {cc -c $< -o $@}
-dep add prog {main.o a.o b.o c.o} {cc $^ -o $@}
+# You can define and use any procedure in tcl.
+proc cc args {exec gcc {*}$args}
 
-# You can add own methods here like this.
-dep method hello args {
+#
+# $self refers [dep]. This may help recursive(multi directory) tasks.
+#
+$self add b.o {b.c b.h} {cc -c $< -o $@}
+
+#
+# $self target add $TARGET depends $DEPENDS action $ACTION
+#
+# is equivalent of:
+#
+# $self add $TARGET $DEPENDS $ACTION
+#
+
+$self target add c.o depends {c.c c.h} action {cc -c $< -o $@}
+$self target add main.o depends {main.c a.h b.h c.h} action {cc -c $< -o $@}
+$self target add prog depends {main.o a.o b.o c.o} action {cc $^ -o $@}
+
+#
+# You can add instance specific methods here like below:
+#
+$self method hello args {
     # puts "$self $type $selfns"
     puts [list HELLO $args]
     return OK
 }
 
-# This works too. But affects other instances too (if it exists).
-snit::method TclTaskRunner hello2 args {
+#
+# You can also add method to snit::type like below:
+#
+snit::method $type hello2 args {
     puts [list Another hello $args]
     return OK2
 }
 
-if {[dep cget -debug]} {
+if {[$self cget -debug]} {
     puts [list ::argv $::argv]
     
-    puts [list targets: [dep target list]]
+    puts [list targets: [$self target list]]
     
-    foreach t [dep target list] {
-        puts [list dependency of $t: {*}[dep dependency list $t]]
+    foreach t [$self target list] {
+        puts [list dependency of $t: {*}[$self dependency list $t]]
     }
 }
 
-dep dispatch $::argv {
+#
+# Some of TclTaskRunner methods are implemented in helper/extras.tcl.
+#
 
-    dep update prog
+#
+# [$self dispatch ARGSVAR DEFAULT ?PHONY_TARGET COMMAND...?] reads given ARGSVAR
+# and dispatches to corresponding target. If no targets are found,
+# it will pass all arguments to $self like [$self {*}$::argv], so that
+# you can invoke arbitrary methods like [$self info methods]...
+#
+$self dispatch $::argv {
+
+    $self update prog
 
 } clean {
 
-    dep run file delete {*}[dep target list]
+    $self run file delete {*}[$self target list]
 
 }
 
-# Equivalent code of [dep dispatch ...]:
+# Equivalent code of [$self dispatch ...]:
 # 
 # if {$::argv eq ""} {
-#      dep update prog
+#      $self update prog
 # 
 # } else {
 #     switch [lindex $::argv 0] {
 #         clean {
-#             file delete {*}[dep target list]
+#             file delete {*}[$self target list]
 #         }
 #         default {
-#             puts [dep {*}$::argv]
+#             puts [$self {*}$::argv]
 #         }
 #     }
 #  }
